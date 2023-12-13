6e791498bbd0a688c2a5ee1ba2f5189b24a08059
tapiwa-1 <tmotsi10@gmail.com>
2023-12-13 10:28:06 +0200

    add nav

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-permissions',
+    '@nuxtjs/tailwindcss',
+    // "./node_modules/flowbite/**/*.{js,ts}"
+    // ...
+  ]
 })
